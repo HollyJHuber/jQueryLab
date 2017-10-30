52c502bf1dc42668ac04777d8e8b1b658c9a341b
HollyJHuber <hollyjhuber@gmail.com>
2017-10-30 14:15:35 -0400

    initial, design/html/css

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+//Ross calls this a document-ready YO, confirms page is loading properly
+// $(function() {
+//     alert("yo");
+// });
+
+// $("ul").hide();
+
+// $("li").filter(":even").css("height", "40px");
+
+// $("p").css("background-color", "rgb(153,153,255");
+
+//  debugger;
+// var btn = $("button");
+// btn.onclick = function() {
+
+// 	$("ul").delay(1200).fadeIn(800);
+
+
+// 	alert("yo");
+// }
+
+
+// $(function() {
+
+// 	$("ul").hide().delay(1200).fadeIn(800);
+// 	$("li").filter(":even").css("height", "40px");
+
+// 	//$("p").css("background-color", "rgb(153,153,255").css("color", "white").css("font-weight", "bold");
+
+// 	// $("p").css({"background-color": "rgb(153,153,255",
+// 	// 	"color": "white", 
+// 	// 	"font-weight": "bold",
+// 	// 	"padding": "24px"});
+
+
+// 	$("p").css({backgroundColor: "rgb(153,153,255)",
+// 		color: "white", 
+// 		fontWeight: "bold",
+// 		padding: "24px"});
+
+// });
+
+
