b9707d40bf538a9086aa04deb5167c111dfb469a
HollyJHuber <hollyjhuber@gmail.com>
2017-10-31 13:30:44 -0400

    close modal, add tableNo to reservation form

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,31 @@
 // });
 
 $(function() {
-	
-	let tableNumber = $("div.circle").text(); 
-	// this returns 123456789
 
-	$("div.circle.available").on("click", function(){
-		$(".formContainer").css('display', 'flex');
+
+	$('.circle.available').on('click', function(){
+		$('.formContainer').css('display', 'flex');
+
+		//return tableNo on reservation form
+		$('.tableNo').text("Table Number: " + $(this).text());
+		$(this).removeClass('available');
 	});
 
+	$('.closeForm').on('click', function(){
+		$(".formContainer").css('display', 'none');
+	});
+
+	$('.button').on('click', function(){
+		$(this).removeClass('available');
+		//$(this).addClass('reserved');
+		$(".formContainer").css('display', 'none');
+	});
+
+
+// after lunch
+// √) remove the input for table number and append the Table Num to the text
+// √) get the damn modal to close
+// _) make submit reserve a table 
 
 });
 // end of wrapping function!!
