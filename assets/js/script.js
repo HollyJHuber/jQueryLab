952880e42a8ee16ee0804f1803329e178f105f77
HollyJHuber <hollyjhuber@gmail.com>
2017-10-31 11:42:13 -0400

    simple form display

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,52 +4,15 @@
 //     alert("yo");
 // });
 
-var tableNumber = $("div.circle").text; 
+$(function() {
+	
+	let tableNumber = $("div.circle").text(); 
+	// this returns 123456789
 
-//this line changes all the numbers to a
-//$("div.circle").text("a
+	$("div.circle.available").on("click", function(){
+		$(".formContainer").css('display', 'flex');
+	});
 
-// this code works if I have an ID set on a table
-var table1 = $("#table1");
 
-table1.on('click', function() {
-
-    table1.text("a");
 });
-
-
-// $("ul").hide();
-
-// $("li").filter(":even").css("height", "40px");
-
-// $("p").css("background-color", "rgb(153,153,255");
-
-
-// 	$("ul").delay(1200).fadeIn(800);
-
-
-// 	alert("yo");
-// }
-
-
-// $(function() {
-
-// 	$("ul").hide().delay(1200).fadeIn(800);
-// 	$("li").filter(":even").css("height", "40px");
-
-// 	//$("p").css("background-color", "rgb(153,153,255").css("color", "white").css("font-weight", "bold");
-
-// 	// $("p").css({"background-color": "rgb(153,153,255",
-// 	// 	"color": "white", 
-// 	// 	"font-weight": "bold",
-// 	// 	"padding": "24px"});
-
-
-// 	$("p").css({backgroundColor: "rgb(153,153,255)",
-// 		color: "white", 
-// 		fontWeight: "bold",
-// 		padding: "24px"});
-
-// });
-
-
+// end of wrapping function!!
